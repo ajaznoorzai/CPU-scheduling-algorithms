7a6fbcc2842a36f8376049baa18cd36850af5a3e
ajaznoorzai <100415304+ajaznoorzai@users.noreply.github.com>
2025-02-17 22:27:57 +0000

    Update FcfsScheduler.java

--- src/main/java/FcfsScheduler.java
+++ src/main/java/FcfsScheduler.java
@@ -2,28 +2,28 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 /**
- * First-Come First-Served Scheduler
+ * First-Come First-Served Scheduler, which is a basic CPU scheduling algorithm where the process that arrives first gets executed first
  * 
  * @author Peter Schrammel
  * @version 2017
  */
-public class FcfsScheduler extends AbstractScheduler {
+public class FcfsScheduler extends AbstractScheduler {   //The FcfsScheduler class extends AbstractScheduler, inherits 
 
-  private Queue<Process> readyQueue;
+  private Queue<Process> readyQueue;   
 
  /**
    * Creates an instance of the FCFS scheduler
    */
   public FcfsScheduler() {
-    readyQueue = new LinkedList<Process>();
+    readyQueue = new LinkedList<Process>();    //Initialises readyQueue as a LinkedList, ensuring that processes are handled in the order they arrive.
     //  System.out.println(readyQueue);
   }
 
   /**
    * Adds a process to the ready queue.
    */
-  public void ready(Process process, boolean usedFullTimeQuantum) {
-    readyQueue.offer(process);
+  public void ready(Process process, boolean usedFullTimeQuantum) {   //ready method it adds a new process in to the queue 
+    readyQueue.offer(process);  //which safely adds the process to the end of the queue.
     //  System.out.println(readyQueue);
   }
 
